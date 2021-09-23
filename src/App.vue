67718b56918a12e08556f8d1ce4159b8e7c214f5
Mauro Nonis <mauro.nonis@gmail.com>
2021-09-23 14:16:05 +0200

    enhanced connection page

--- src/App.vue
+++ src/App.vue
@@ -20,17 +20,6 @@
           width="100"
         />
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,80 @@
 <template>
-  <v-card class="ma-6">
-    <v-toolbar text color="primary" dark>
-      <v-toolbar-title>MQTT Connection</v-toolbar-title>
-    </v-toolbar>
-    <v-btn text v-on:click="connectToMqtt">Connect</v-btn>
-  </v-card>
+  <v-container class="connection-container">
+    <v-card>
+      <v-toolbar text color="primary" dark>
+        <v-toolbar-title>MQTT Connection</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+        <v-form>
+          <div class="input-container">
+            <v-text-field v-model="name" label="Name" required></v-text-field>
+            <div row>
+              <v-text-field v-model="host" label="Host" required></v-text-field>
+              <v-text-field v-model="port" label="Port" required></v-text-field>
+            </div>
+            <div row>
+              <v-text-field
+                v-model="username"
+                label="Username"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                label="Password"
+                required
+              ></v-text-field>
+            </div>
+          </div>
+          <v-btn text v-on:click="connectToMqtt">Connect</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
+
+<style scoped>
+.connection-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+}
+
+div[row] {
+  display: flex;
+  flex-direction: row;
+  gap: 2em;
+}
+</style>
 
 <script>
 export default {
   name: "Home",
 
-  data: () => ({}),
+  data: () => ({
+    name: undefined,
+    host: undefined,
+    port: undefined,
+    username: undefined,
+    password: undefined,
+  }),
 
   methods: {
     connectToMqtt() {
+      let connection = {
+        name: this.name,
+        host: this.host,
+        port: this.port,
+        username: this.username,
+        password: this.password,
+      };
+      this.$store.commit("addNewConnection", connection);
       this.$router.push({ name: "Viewer" });
     },
   },
