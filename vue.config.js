694174c43b8980318637fcb289d542d5ab6198a3
Leonardo Cossutta <leonardo.cossutta@gmail.com>
2021-10-22 10:16:08 +0200

    Update .gitignore, entitlements.mac.plist, and vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -50,6 +50,7 @@ module.exports = {
         },
         mac: {
           category: builderOpts.appCategories.mac,
+          electronLanguages: ["en"],
           target: ["dmg"],
         },
         win: {
