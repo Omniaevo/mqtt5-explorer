c3ced0aab2c928b910580adf7887defe8f1863a1
Leonardo Cossutta <leonardo.cossutta@gmail.com>
2022-03-21 16:44:18 +0100

    Update background.js

--- src/background.js
+++ src/background.js
@@ -137,6 +137,7 @@ async function createWindow() {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      nodeIntegrationInWorker: process.env.ELECTRON_NODE_INTEGRATION,
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
       nativeWindowOpen: true,
     },
