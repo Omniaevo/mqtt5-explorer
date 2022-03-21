af6bc4119f4ba219b4c4c6b5205a8001e61cc8c1
Leonardo Cossutta <leonardo.cossutta@gmail.com>
2022-03-21 09:46:52 +0100

    fix deprecation warning

--- src/background.js
+++ src/background.js
@@ -116,6 +116,7 @@ async function createWindow() {
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
+      nativeWindowOpen: true,
     },
   });
 
